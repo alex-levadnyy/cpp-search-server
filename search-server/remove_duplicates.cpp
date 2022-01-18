1e4fc302ea132bd966ba3e361a0556e2d38f434e
alex-levadnyy <93546496+alex-levadnyy@users.noreply.github.com>
2022-01-18 20:21:35 +0300

    по замечаниям ревьюера

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,13 +1,14 @@
 #include "remove_duplicates.h"
-#include <iostream>
 #include <string>
 #include <vector>
+#include <set>
+#include <iostream>
 
 using namespace std::literals;
 
 void RemoveDuplicates(SearchServer& search_server) {
 	std::vector<int> id_document_for_delete;
-	std::vector<std::vector<std::string>> word_doc;
+	std::set<std::vector<std::string>> word_doc;
 	for (int id_document : search_server) {
 		const auto& id_word_freq = search_server.GetWordFrequencies(id_document);
 		std::vector<std::string> temp;
@@ -15,15 +16,13 @@ void RemoveDuplicates(SearchServer& search_server) {
 			temp.push_back(word.first);
 		}
 		if (word_doc.empty()) {
-			word_doc.push_back(temp);
+			word_doc.insert(temp);
 		}
 		else {
-			auto result = std::find(word_doc.cbegin(), word_doc.cend(), temp);
-			if (result != word_doc.cend()) {
+			size_t size_befor_add = word_doc.size();
+			word_doc.insert(temp);
+			if (size_befor_add == word_doc.size()) {
 				id_document_for_delete.push_back(id_document);
-			}
-			else {
-				word_doc.push_back(temp);
 			}
 		}
 		temp.clear();
@@ -32,5 +31,4 @@ void RemoveDuplicates(SearchServer& search_server) {
 		search_server.RemoveDocument(id_document_for_delete[i]);
 		std::cout << "Found duplicate document id "s << id_document_for_delete[i] << std::endl;
 	}
-	
 }
